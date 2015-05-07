ac4810daafe8dfd82c7b2f18f58bccac2216dee6
Rafael Teixeira <monoman@gmail.com>
2015-05-07 14:24:53 -0300

    Seems that now nuget is part of mono's image on travis-ci

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-mkdir -p ~/bin && PATH="~/bin;$PATH" && echo === Adjusted PATH: $PATH && \
-curl -sSL https://nuget.org/nuget.exe -o ~/bin/nuget.exe && echo === Downloaded nuget.exe && \
-echo '#!/bin/bash' > nuget && echo "mono `which nuget.exe`" >> nuget && chmod +x nuget && \
-mv -f nuget ~/bin/ && echo === Wrapping script for nuget.exe 'nuget' created: && cat ~/bin/nuget && \
-nuget | head -n 2 && echo === Starting build... && \
+echo === Verifying nuget existence: && nuget && echo === Starting build... && \
 xbuild Commons.Core.csproj
