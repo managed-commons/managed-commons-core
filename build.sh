5da1abefe6dc137b56314e26233a0dcb3519d985
Rafael Teixeira <monoman@gmail.com>
2015-05-07 13:31:21 -0300

    Another take on obtaining nuget

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+curl -sSL https://nuget.org/nuget.exe -o nuget.exe && echo Downloaded nuget.exe && \
+mkdir -p ~/bin && PATH="~/bin;$PATH" && echo $PATH && \
+chmod +x nuget.sh && \
+ln -T -s nuget.sh ~/bin/nuget && \
 xbuild Commons.Core.csproj
