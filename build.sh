8d924c5021245a678fa370946dff4dd79530fccb
Rafael Teixeira <monoman@gmail.com>
2015-05-07 14:06:17 -0300

    Better nuget wrapping

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-curl -sSL https://nuget.org/nuget.exe -o nuget.exe && echo Downloaded nuget.exe && \
-mkdir -p ~/bin && PATH="~/bin;$PATH" && echo $PATH && \
-chmod +x nuget.sh && \
-ln -T -s nuget.sh ~/bin/nuget && \
+mkdir -p ~/bin && PATH="~/bin;$PATH" && echo === Adjusted PATH: $PATH && \
+curl -sSL https://nuget.org/nuget.exe -o ~/bin/nuget.exe && echo === Downloaded nuget.exe && \
+echo '#!/bin/bash' > nuget && echo "mono `which nuget.exe`" >> nuget && chmod +x nuget && \
+mv -f nuget ~/bin/ && echo === Wrapping script for nuget.exe 'nuget' created: && cat ~/bin/nuget && \
+nuget | head -n 2 && echo === Starting build... && \
 xbuild Commons.Core.csproj
