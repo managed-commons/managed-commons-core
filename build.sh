ff3b035da02438bcab89ab68f0284d0c5a56c3b0
Rafael Teixeira <monoman@gmail.com>
2015-05-07 15:14:10 -0300

    Again trying to download nuget.exe
    the one available in travis-ci says:
    Attempting to build package from 'Commons.Core.csproj'.
    The requested feature is not implemented.

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-xbuild /property:DoNotPack=true Commons.Core.csproj && nuget pack Commons.Core.csproj
+echo === Downloading nuget.exe && curl -sSL https://nuget.org/nuget.exe -o nuget.exe && \
+echo === Building the library && xbuild /property:DoNotPack=true Commons.Core.csproj && \
+echo === Packing the nuget && mono nuget.exe pack Commons.Core.csproj
