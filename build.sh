8e5c23668a3c49c028a75dbfb94ddb6829b4132f
Rafael Teixeira <monoman@gmail.com>
2015-05-07 15:20:15 -0300

    Give up nuget packing on travis-ci

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-echo === Downloading nuget.exe && curl -sSL https://nuget.org/nuget.exe -o nuget.exe && \
-echo === Building the library && xbuild /property:DoNotPack=true Commons.Core.csproj && \
-echo === Packing the nuget && mono nuget.exe pack Commons.Core.csproj
+echo === Building the library && xbuild /property:DoNotPack=true Commons.Core.csproj
