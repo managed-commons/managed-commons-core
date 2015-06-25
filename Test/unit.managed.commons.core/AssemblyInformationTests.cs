d8c7bc59e67d31aa1165e3247ee35569e9551ee9
Rafael Teixeira <monoman@gmail.com>
2015-06-25 11:31:39 -0300

    New unit test for AssemblyInformation.ToString()

---
+++ Test/unit.managed.commons.core/AssemblyInformationTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Commons.Reflection;
+
+namespace Commons.Core
+{
+    public class AssemblyInformationTests
+    {
+        [Fact]
+        public void ToStringOnSimpleAssemblyInfo()
+        {
+            Translation.TranslationService.Locale = "en-US";
+            var info = new AssemblyInformation(typeof(AssemblyInformation).Assembly);
+            Assert.Equal(
+                "Commons.Core  2.0.0-Alpha - Copyright ©2002-2015 Rafael 'Monoman' Teixeira, Managed Commons Team\r\n"+
+                "Core meta-information library\r\n\r\n"+
+                "License: MIT License - Mais detalhes em https://opensource.org/licenses/MIT\n\r\n\r\n\r\n"+
+                "Authors: Rafael 'Monoman' Teixeira, Managed Commons Team\r\n", 
+                info.ToString());
+        }
+    }
+}
